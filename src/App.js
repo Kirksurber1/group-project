a94978ab8c202a672c53b8d0717e8f2fa941d328
Kirk Surber <kirk.surber@surberphotography.com>
2023-01-28 15:12:16 -0600

    updated App js file and MemeForm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import Meme from './components/Meme'
 import './style.css'
 import MemeForm from './MemeForm'
 import './MemeForm.css'
 
 export default function App(){
+
+    const [memeArray, setMemeArray] = React.useState([])
+
+
+    function addMeme (memes) {
+        setMemeArray((prevMemeArray) => {
+            return [memes, ...prevMemeArray]
+        }) 
+    }
+    
+
+
+
     return(
         <div>
-            <h1>App</h1>
-            <MemeForm/>
+            <Meme 
+                topText='One does not simply'
+                bottomText='Walk into Mordor'
+                image='http://i.imgflip.com/1bij.jpg'
+            />
         </div>
     )
 }
