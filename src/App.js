c7296013d6ec377534be324f218964b447d9ea19
Kirk Surber <kirk.surber@surberphotography.com>
2023-01-28 14:16:46 -0600

    minor app imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './style.css'
+import MemeForm from './MemeForm'
+import './MemeForm.css'
 
 export default function App(){
     return(
         <div>
             <h1>App</h1>
+            <MemeForm/>
         </div>
     )
 }
